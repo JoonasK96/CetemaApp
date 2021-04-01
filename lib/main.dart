4f3c2cddc7c4f27e5d154880bf258fbddd275135
ramezzi <rattimestari@gmail.com>
2021-04-01 12:51:34 +0300

    firebase commit mukana unohtuneet palaset

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'components/navigation.dart';
+//import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'dart:async';
+//import 'dart:math';
 
-void main() => runApp(MyApp());
+//void main() => runApp(MyApp()); korvasin tän tolla alemmalla t. Otto
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  //Firebase initti
+  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
