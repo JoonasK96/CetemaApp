150d377000b6afafe117ccb41bd03a52c9cae272
teemusalo1 <teemu.matias.salo@gmail.com>
2021-04-06 12:30:32 +0300

    trying to fix CI workflow

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'components/navigation.dart';
 //import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_core/firebase_core.dart';
+//import 'package:firebase_core/firebase_core.dart';
 import 'dart:async';
 //import 'dart:math';
 
