c9a532cd30e4527ee78f6a5a70422df08139093b
teemusalo1 <teemu.matias.salo@gmail.com>
2021-04-06 12:49:13 +0300

    trying to fix CI workflow

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 //import 'package:flutter_app/components/forecast_card.dart';
-import 'package:flutter_app/components/weather_card.dart';
+//import 'package:flutter_app/components/weather_card.dart';
 
 class WeatherScreen extends StatelessWidget {
   @override
